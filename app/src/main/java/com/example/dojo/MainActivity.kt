146c7f2b02023b382f3fc79f0b6a86281087ff94
Anton Arbuzov <anton0994pacan@gmail.com>
2025-09-20 12:47:49 +0300

    Pixel 7 (API36.0)

---
+++ app/src/main/java/com/example/dojo/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.dojo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dojo.ui.theme.DojoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DojoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("Hello $name!") }
+
+    Column(modifier = modifier) {
+        Text(text = text)
+        Button(onClick = { text = "Текст изменён!" }) {
+            Text("Нажми меня")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DojoTheme {
+        Greeting("Android")
+    }
+}
